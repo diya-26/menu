d3a0a4ed76f44e6fedf9c3b14847120692b2ef43
diya <diyakhandelwal26@gmail.com>
2023-10-15 23:31:13 +0530

    files added

---
+++ src/components/Basics/MenuApi.js
@@ -0,0 +1,87 @@
+const Menu = [
+  
+    {
+      id: 1,
+      image: "images/maggie.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Maggie offers a wide range of delicious flavors to suit different tastes, from classic masala to exotic international options. There's something for everyone.",
+    },
+  
+    {
+      id: 2,
+      image: "images/allu.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "This snack is a testament to the beauty of simplicity, with thinly sliced potatoes coated in a seasoned gram flour batter and deep-fried to perfection.",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "This dish features bright yellow corn kernels simmered in a luscious tomato-based gravy, creating a visually appealing and colorful meal.",
+    },
+    {
+      id: 4,
+      image: "../images/cholek.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        " This dish offers the perfect harmony of spicy and tangy chickpea curry (chole) paired with soft, fluffy, and slightly crispy bread (kulche).",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Pizza is the epitome of versatility, with endless topping combinations that cater to every palate, from classic Margherita to inventive gourmet varieties.",
+    },
+    {
+      id: 6,
+      image: "../images/non.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Exploring non-veg dishes is like embarking on a culinary adventure, where flavors, textures, and cooking methods vary widely across cultures.",
+    },
+    {
+      id: 7,
+      image: "../images/aweets.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "From chocolates and candies to pastries and traditional desserts, sweets come in a delightful array of flavors and textures.",
+    },
+    {
+      id: 8,
+      image: "../images/rajma.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma and rice make the perfect pairing, with the creamy beans complementing the fluffy grains for a wholesome meal.",
+    },
+    {
+      id: 9,
+      image: "../images/sam.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        " Samosas are savory, triangular pastries that tantalize the taste buds with their crispy, golden exteriors and flavorful fillings.",
+    },
+  ];
+  
+  export default Menu;
---
+++ src/components/Basics/Resturant.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import "./style.css";
+import Menu from "./MenuApi.js";
+import MenuCard from './menuCard'; // Corrected component name
+
+const uniqueList=[...new Set(
+  Menu.map((curElem)=>{
+    return curElem.category 
+  })
+)]
+const Restaurant = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const filterItem=(category) =>{
+    const updatedList= Menu.filter((curElem) =>{
+      return curElem.category ===category;
+
+    });
+    setMenuData(updatedList);
+  };
+  return (
+    <>
+        <nav className='navbar'>
+          <div className='btn-group'>
+            <button className='btn-group__item' onClick={()=> filterItem("breakfast")}>Breakfast</button>
+            <button className='btn-group__item' onClick={()=> filterItem("lunch")}>lunch</button>
+            <button className='btn-group__item' onClick={()=> filterItem("evening")}>evening</button>
+            <button className='btn-group__item' onClick={()=> filterItem("dinner")}>dinner</button>
+            <button className='btn-group__item' onClick={()=> setMenuData(Menu)}>All</button>
+          </div>
+        </nav>
+      <MenuCard menuData={menuData}/> 
+    </>
+  );
+};
+
+export default Restaurant;
